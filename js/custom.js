6071a3fff191a2682a573ecd393c445271c8f692
“wanglin26782” <wanglin26782@163.com>
2017-11-01 23:44:08 +0800

    添加可视化展示页面

--- js/custom.js
+++ js/custom.js
@@ -4,5 +4,22 @@
 
 $(function () {
 
+	//可视化展示侧边栏
+	var visualBtn = $('.rx-visual .rx-sidebar ul li a');
+
+	visualBtn.click(function () {
+		$(this).addClass('active').parent().siblings().children().removeClass('active');
+	});
+
+	/*复制代码*/
+	var clipboard = new Clipboard('.rx-page-component a.copy');
+
+	clipboard.on('success', function (e) {
+		console.log(e)
+	});
+
+	clipboard.on('error', function (e) {
+		console.log(e);
+	});
 
 });
