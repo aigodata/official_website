10b8c37c3d2870846833ac51ff287b76dcc3da32
tm-roamer <tm165822@163.com>
2017-11-15 22:44:22 +0800

    更新官网链接

---
+++ visual/ag-graph/graph-data.js
@@ -0,0 +1,81 @@
+var graphData = {
+	"lines": [
+		{
+			"id": "l-custom-0", "source": "n-custom-0", "target": "n-custom-1",
+			"pointsData": [
+				{ "x": -67, "y": -168.20936273576945 }, { "x": -67, "y": -148 }, { "x": -156.4921875, "y": -148 }, { "x": -156.92637903491683, "y": -128.79063726423055 }
+			]
+		},
+		{
+			"id": "l-custom-1", "source": "n-custom-0", "target": "n-custom-2",
+			"pointsData": [
+				{ "x": -56, "y": -168.21233959703564 }, { "x": -56, "y": -148 }, { "x": 34.5078125, "y": -147 }, { "x": 33.7510317233588, "y": -126.58766040296436 }
+			]
+		},
+		{
+			"id": "l-custom-2", "source": "n-custom-1", "target": "n-custom-3",
+			"pointsData": [
+				{ "x": -156.2494723492485, "y": -83.84701983781612 }, { "x": -157.4921875, "y": -16 }, { "x": -83.44855424465808, "y": -16.518184362638856 }
+			]
+		},
+		{
+			"id": "l-custom-3", "source": "n-custom-2", "target": "n-custom-3",
+			"pointsData": [{ "x": 33.12242455206034, "y": -83.93695668999507 }, { "x": 32.5078125, "y": -14 }, { "x": -30.606263000406244, "y": -14.617843999589297 }]
+		},
+		{
+			"id": "l-custom-4", "source": "n-custom-3", "target": "n-custom-4",
+			"pointsData": [
+				{ "x": -63.53449590209334, "y": 9.944837049507196 }, { "x": -63.4921875, "y": 30 }, { "x": -182.4921875, "y": 30 }, { "x": -182.52317580516413, "y": 54.976686579718184 }
+			]
+		},
+		{
+			"id": "l-custom-5", "source": "n-custom-5", "target": "n-custom-3",
+			"pointsData": [
+				{ "x": 34.02943725152286, "y": 58.029437251522864 }, { "x": 33.5078125, "y": 31 }, { "x": -50.4921875, "y": 30 }, { "x": -51.49390334559011, "y": 10.506096654409877 }
+			]
+		},
+		{
+			"id": "l-custom-6", "source": "n-custom-5", "target": "n-custom-9",
+			"pointsData": [
+				{ "x": 29.576768565481764, "y": 95.89514659044667 }, { "x": 29.5078125, "y": 123 }, { "x": -18.4921875, "y": 123 }, { "x": -18.373275114339734, "y": 151.2496756158964 }
+			]
+		},
+		{
+			"id": "l-custom-7", "source": "n-custom-5", "target": "n-custom-10",
+			"pointsData": [
+				{ "x": 43.90509227327365, "y": 96.56140099460531 }, { "x": 43.5078125, "y": 123 }, { "x": 97.5078125, "y": 125 }, { "x": 96.99180195032503, "y": 149.5138908792708 }
+			]
+		},
+		{
+			"id": "l-custom-8", "source": "n-custom-4", "target": "n-custom-7",
+			"pointsData": [
+				{ "x": -183.12959726326986, "y": 98.91656263193883 }, { "x": -183.4921875, "y": 130 }, { "x": -207.4921875, "y": 130 }, { "x": -208.35573549991136, "y": 188.8577212497151 }
+			]
+		},
+		{
+			"id": "l-custom-9", "source": "n-custom-4", "target": "n-custom-8",
+			"pointsData": [
+				{ "x": -169.98822402323196, "y": 98.48512596769305 }, { "x": -169.4921875, "y": 120 }, { "x": -114.4921875, "y": 121 }, { "x": -115.09265598160803, "y": 148.57427527557633 }
+			]
+		},
+		{
+			"id": "l-custom-10", "source": "n-custom-4", "target": "n-custom-6",
+			"pointsData": [
+				{ "x": -196.1419363713435, "y": 98.47709473462952 }, { "x": -196.4921875, "y": 120 }, { "x": -299.4921875, "y": 120 }, { "x": -299.84596703935307, "y": 149.5389666684183 }
+			]
+		}],
+	"nodes": [
+		{ "id": "n-custom-0", "text": "", "image": "./images/router.svg", "size": 40, "badge": "", "info": "", "x": -62, "y": -189 }, { "id": "n-custom-1", "text": "", "image": "./images/firewall.svg", "size": 40, "badge": "", "info": "", "x": -158, "y": -106 }, { "id": "n-custom-2", "text": "", "image": "./images/firewall.svg", "size": 40, "badge": "", "info": "", "x": 33, "y": -107 }, { "id": "n-custom-3", "text": "hub", "image": "./images/switch.svg", "size": 50, "badge": "", "info": "", "x": -58, "y": -15 }, { "id": "n-custom-4", "text": "", "image": "./images/server.svg", "size": 40, "badge": "", "info": "", "x": -183, "y": 75 }, { "id": "n-custom-5", "text": "", "image": "./images/server.svg", "size": 40, "badge": "", "info": "", "x": 37, "y": 75 }, { "id": "n-custom-6", "text": "", "image": "./images/pc.svg", "size": 30, "badge": "", "info": "", "x": -302, "y": 165 }, { "id": "n-custom-7", "text": "", "image": "./images/pc.svg", "size": 30, "badge": "2", "info": "", "x": -211, "y": 207 }, { "id": "n-custom-8", "text": "", "image": "./images/pc.svg", "size": 30, "badge": "", "info": "", "x": -119, "y": 165 }, { "id": "n-custom-9", "text": "", "image": "./images/pc.svg", "size": 30, "badge": "1", "info": "", "x": -18, "y": 167 }, { "id": "n-custom-10", "text": "", "image": "./images/pc.svg", "size": 30, "badge": "", "info": "", "x": 96, "y": 166 }
+	],
+	paths: [
+		{
+			source: "n-custom-7", target: "n-custom-0", class: ["status-1"], repeat: true,
+		},
+		{
+			source: "n-custom-7", target: "n-custom-1", class: ["status-0"], repeat: false
+		},
+		{
+			source: "n-custom-9", target: "n-custom-0", class: ["status-3"], repeat: true
+		}
+	]
+}
